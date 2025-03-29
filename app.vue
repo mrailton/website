0e7db8aeb318914be4fb677932d9849005858abc
Mark Railton <mark.railton@outlook.ie>
2025-03-29 15:26:01 +0000

    Making a start at adding SEO

--- app.vue
+++ app.vue
@@ -1,3 +1,12 @@
+<script setup lang="ts">
+useSeoMeta({
+  title: 'Mark Railton',
+  ogTitle: 'Mark Railton',
+  description: 'Personal website and blog of Mark Railton.',
+  ogDescription: 'Personal website and blog of Mark Railton.',
+});
+</script>
+
 <template>
   <NuxtLayout>
     <NuxtPage />
